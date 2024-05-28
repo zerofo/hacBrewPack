818365a19b8618f300c4d000f0987e8a0e4f95cf
lavender hearts <hearts@colbatic.com>
2024-05-28 13:45:27 -0400

    feat: nixpkg building support

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ stdenv }:
+
+stdenv.mkDerivation rec {
+  pname = "hacBrewPack";
+  version = "3.15";
+
+  src = ./src;
+
+  patches = [];
+
+  preBuild = ''
+    mv config.mk.template config.mk
+  '';
+
+  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+  enableParallelBuilding = true;
+
+  installPhase = ''
+    install -D hacbrewpack $out/bin/hacbrewpack
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/gayhearts/hacBrewPack";
+    description = "Make NCAs/NSPs from Nintendo Switch homebrews";
+    longDescription = "hacBrewPack is a tool for creating Nintendo Switch NCAs (Nintendo Content Archive) from homebrews and pack them into NSPs (Nintendo Submission Package)
+    Thanks: SciresM, yellows8, Adubbz, SwitchBrew";
+    license = licenses.gpl2;
+    maintainers = with maintainers; [ ivar ];
+    platforms = platforms.unix;
+    mainProgram = "hacBrewPack";
+  };
+}
