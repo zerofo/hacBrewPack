1a5f378c1b5747c603f4a50a4a97d86cc7c05fd4
lavender hearts <hearts@colbatic.com>
2024-05-28 18:13:30 -0400

    fix: first working default.nix

--- default.nix
+++ default.nix
@@ -1,29 +1,33 @@
-{ stdenv, ... }:
+{ system ? builtins.currentSystem }:
 
-stdenv.mkDerivation rec {
-  pname = "hacBrewPack";
-  version = "3.16";
+let
+  pkgs = import <nixpkgs> { inherit system; };
+in
+  with import <nixpkgs> {};
+	stdenv.mkDerivation rec {
+	  pname = "hacBrewPack";
+	  version = "3.17";
 
-  src = ./src;
+	  src = ./src;
 
-  patches = [];
+	  patches = [];
 
-  preBuild = ''
-    mv config.mk.template config.mk
-  '';
+	  preBuild = ''
+		mv config.mk.template config.mk
+	  '';
 
-  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
-  enableParallelBuilding = true;
+	  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+	  enableParallelBuilding = true;
 
-  installPhase = ''
-    install -D hacbrewpack $out/bin/hacbrewpack
-  '';
+	  installPhase = ''
+		install -D hacbrewpack $out/bin/hacbrewpack
+	  '';
 
-  meta = {
-    homepage = "https://github.com/gayhearts/hacBrewPack";
-    description = "Make NCAs/NSPs from Nintendo Switch homebrews";
-    longDescription = "hacBrewPack is a tool for creating Nintendo Switch NCAs (Nintendo Content Archive) from homebrews and pack them into NSPs (Nintendo Submission Package)
-    Thanks: SciresM, yellows8, Adubbz, SwitchBrew";
-    mainProgram = "hacBrewPack";
-  };
-}
+	  meta = with lib; {
+		homepage = "https://github.com/gayhearts/hacBrewPack";
+		description = "Make NCAs/NSPs from Nintendo Switch homebrews";
+		longDescription = "hacBrewPack is a tool for creating Nintendo Switch NCAs (Nintendo Content Archive) from homebrews and pack them into NSPs (Nintendo Submission Package)
+		Thanks: SciresM, yellows8, Adubbz, SwitchBrew";
+		mainProgram = "hacBrewPack";
+	  };
+	}
